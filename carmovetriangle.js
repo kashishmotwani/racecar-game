086460fd361ecca3a1720d576b3976c10fb54a96
m-ahm <56158441+m-ahm@users.noreply.github.com>
2019-11-17 19:23:14 +0400

    Update carmovetriangle.js

--- carmovetriangle.js
+++ carmovetriangle.js
@@ -1,20 +1,20 @@
 /* to be looped for the car to be appear to be moving */
 
 // if the upper key is pressed
-  if (controller.up) {
+  if (inputHandler.up) {
     triangle.y_velocity -= 0.5;
   }
 
   // if the left key is pressed
-  if (controller.left) {
+  if (inputHandler.left) {
     triangle.x_velocity -= 0.5;
   }
   // if the right key is pressed
-  if (controller.right) {
+  if (inputHandler.right) {
     triangle.x_velocity += 0.5;
   }
   // if the down key is pressed
-  if (controller.down) {
+  if (inputHandler.down) {
     triangle.y_velocity += 0.5;
   }
 
