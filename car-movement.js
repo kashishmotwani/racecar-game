53e9b78db1bc52d716e76bee344db543cf2d06c9
m-ahm <56158441+m-ahm@users.noreply.github.com>
2019-11-25 16:47:26 +0400

    Update car-movement.js

--- car-movement.js
+++ car-movement.js
@@ -1,4 +1,4 @@
-var context, inputHandler, rectangle, loop, car;
+var context, inputHandler, loop, car;
 
 context = document.querySelector("canvas").getContext("2d");
 
@@ -7,37 +7,11 @@ context.canvas.width = 1200;
 
 car = document.getElementById("car");
 
-car.style.top = 75 + 'px';
-car.style.left = 387 + 'px';
-
-rectangle = {
-  height:20,
-  width:20,
-  x:180, // location @ center of the canvas
-  x_speed:0,
-  y: 160,
-  y_speed:0,
-};
-
-rectangle2 = {
-  height:20,
-  width:20,
-  x:180, // location @ center of the canvas
-  x_speed:0,
-  y: 130,
-  y_speed:0,
-};
-
-rectangle3 = {
-  height:20,
-  width:20,
-  x:180,        // location @ center of the canvas; x-axis
-  x_speed:0, // speed; x-axis
-  y: 100,       // location @ center of the canvas; y-axis
-  y_speed:0, // speed; x-axis
-};
+car.style.top = 75 + 'px'; // location of the car on the canvas
+car.style.left = 387 + 'px'; // location of the car on the canvas
 
 inputHandler = {
+  // handles the controls of the arrow keys
 
   left: false,
   right: false,
@@ -67,31 +41,8 @@ inputHandler = {
 
 };
 
-loop = function() {
-  
-  if (inputHandler.up) {
-    rectangle.y_speed -= 0.5;
-  }
+  // Car boundaries - IMPLEMENTED BUT NOT WORKING AS INTENDED
 
-  if (inputHandler.left) {
-    rectangle.x_speed -= 0.5;
-  }
-
-  if (inputHandler.right) {
-    rectangle.x_speed += 0.5;
-  }
-
-  if (inputHandler.down) {
-    rectangle.y_speed += 0.5;
-  }
-  rectangle.x += rectangle.x_speed;
-  rectangle.y += rectangle.y_speed;
-  rectangle.x_speed *= 0.9;// friction
-  rectangle.y_speed *= 0.9;// friction
-  //console.log("x is " + rectangle.x)
-  //console.log("y is " + rectangle.y)
-
-  // Car boundaries
   // x-axis -> horizontal, y-axis -> vertical
   //40 -> y top of race track
   //714.9 -> y bottom of race track
@@ -99,118 +50,65 @@ loop = function() {
   //959.9 -> 867 px-> x right most section of the track
   //340 -> y bottom of the upper part of the track
   //460 -> x upper of the bottom part of the track
-  //725/719.5 -> x at the curve in the middle of the track connecting the bottom part with the top part
-  //955 -> x end of the 
+
   // boundaries
-  if (rectangle.x <= 170) {
-    rectangle.x_speed = 0
-    rectangle.x = 170
-  } else if (rectangle.x >= 959.9) {
-    rectangle.x_speed = 0
-    rectangle.x = 959.9
+  /*if((car1.style.top < (46) + 'px') && (car1.style.left < (177) + 'px'))  {
+    car1.style.left = (177) + 'px';
+    car1.style.top = (46) + 'px';
   }
-  //else if (rectangle.x >= 959.9) {
-    //rectangle.x_speed = -rectangle.x_speed
-   else if (rectangle.y < 40) {
-    rectangle.y_speed = 0
-    rectangle.y = 40
-  } else if (rectangle.y >= 714.9){
-    rectangle.y_speed = 0
-    rectangle.y = 714.9}
-    /* you can leave the middle part with rectangle.x_speed = -rectangle.x_speed and make it as the hard part
-    where the speed of the car decreases and all?
-  } else if (rectangle.y >= 340) { //the rest
-    rectangle.y_speed = 0
-    rectangle.y = 340
-  } else if (rectangle.y >= 460) {
-    rectangle.y_speed = 0
-    rectangle.y = 460
-  } else if  (rectangle.y >= 340 && rectangle.x >= 180) {
-    rectangle.y_speed = 0
-    rectangle.y = rectangle.y
-    rectangle.x = rectangle.x 
-    //rectangle.x = 180
-  }else if (rectangle.y >= 340 && rectangle.y <= 460 && rectangle.x >= 180 && rectangle.x <= 719.5) {
-    rectangle.y_speed = 0
-    rectangle.x_speed = 0
-    //rectangle.x = 719.5
+  else if(car1.style.left > (961) + 'px' && car1.style.top < (46) + 'px')  {
+    car1.style.left = (961) + 'px';
+    car1.style.top = (46) + 'px';
+  }
+  else if(car1.style.left > (961) + 'px' && car1.style.top > (718) + 'px')  {
+    car1.style.left = (961) + 'px';
+    car1.style.top = (718) + 'px';
+  }
+  else if(car1.style.left < (177) + 'px' && car1.style.top > (718) + 'px')  {
+    car1.style.left = (177) + 'px';
+    car1.style.top = (718) + 'px';
+    
+  if(car1.style.top <= (46) + 'px')  { //working
+    car1.style.top = (46) + 'px';
+  }
+  else if(car1.style.left < (177) + 'px')  {
+    car1.style.left = (177) + 'px';
+  }
+  else if(car1.style.top > (718) + 'px' && car1.style.left)  {
+    car1.style.top = (718) + 'px';
+  }
+  else if(car1.style.left > (961) + 'px')  {
+    car1.style.left = (961) + 'px';
   }*/
-  //else if (rectangle.y >= 714.9) {
-    //rectangle.y_speed = -rectangle.y_speed
-  
 
-
-
-  // if rectangle is going off the left of the screen
-  if (rectangle.x < -32) {
-    rectangle.x = 1190;
-// if rectangle goes past right boundary
-  } else if (rectangle.x > 1190) {
-    rectangle.x = -32;
-// if rectangle goes past lower boundary
-  } else if (rectangle.y > 790) {
-    rectangle.y = 0;
-// if rectangle goes past upper boundary
-  } else if (rectangle.y < -32) {
-    rectangle.y = 790;
-  }
-  
-  //src:https://stackoverflow.com/questions/10791610/javascript-html5-using-image-to-fill-canvas
-  //var background = document.getElementById("background");
-  var background = new Image();
-  background.src = "race-track.png"; 
-  background.onload = function(){
-      var pattern = context.createPattern(this, "repeat");
-      context.fillStyle = pattern;
-      context.fill();
-  };
-
-  context.fillRect(0, 0, 1200, 800);// x, y, width, height
-  context.fillStyle = "white";
-  context.beginPath();
-  context.rect(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
-  context.fill();
-  
-  context.beginPath();
-  context.rect(rectangle2.x, rectangle2.y, rectangle2.width, rectangle2.height);
-  context.fill();
-  
-  context.beginPath();
-  context.rect(rectangle3.x, rectangle3.y, rectangle3.width, rectangle3.height);
-  context.fill();
-
-  // call update when the browser is ready to draw again
-  window.requestAnimationFrame(loop);
-
-};
-
-loop2 = function() {
+loop = function() {
+  // the function loop requests the browser to update the animation with every movement of the car
   if (inputHandler.up) {
     car.style.top = (car.offsetTop -= 7) + 'px';
+    // rotation of car
     direction = document.getElementById("car").style.transform = "rotate(270deg)"
   }
 
   if (inputHandler.left) {
     car.style.left = (car.offsetLeft -= 7) + 'px';
+    // rotation of car
     direction = document.getElementById("car").style.transform = "rotate(180deg)"
   }
 
   if (inputHandler.right) {
     car.style.left = (car.offsetLeft += 7) + 'px';
+    // rotation of car
     direction = document.getElementById("car").style.transform = "rotate(0deg)"
 
   }
 
   if (inputHandler.down) {
     car.style.top = (car.offsetTop += 7) + 'px';
+    // rotation of car
     direction = document.getElementById("car").style.transform = "rotate(90deg)"
   }
   car.style.left += (car.style.left) + 'px';
   car.style.top += (car.style.top) + 'px';
-  console.log("left is " + car.style.left)
-  console.log("top is " + car.style.top)
-  console.log("x is " + rectangle.x)
-  console.log("y is " + rectangle.y)
 
   var background = new Image();
   background.src = "race-track.png"; 
@@ -221,9 +119,8 @@ loop2 = function() {
   };
   context.fillRect(0, 0, 1200, 800);// x, y, width, height
   context.fillStyle = "black";
-  window.requestAnimationFrame(loop2);
+  window.requestAnimationFrame(loop);
 }
 window.addEventListener("keydown", inputHandler.keyListener) //press down and it moves
 window.addEventListener("keyup", inputHandler.keyListener);  //lift finger and it stops
-//window.requestAnimationFrame(loop)
-window.requestAnimationFrame(loop2);
+window.requestAnimationFrame(loop);
